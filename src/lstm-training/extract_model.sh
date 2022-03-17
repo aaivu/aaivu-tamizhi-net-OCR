0147cfd3c45ebf925b8274d08878084838393131
Charangan Vasantharajan <46817693+Chaarangan@users.noreply.github.com>
2022-03-17 13:17:40 -0700

    Update extract_model.sh

--- src/lstm-training/extract_model.sh
+++ src/lstm-training/extract_model.sh
@@ -1,3 +1,5 @@
+#Author: Charangan Vasantharajan
+
 combine_tessdata -e data/tessdata/tam.traineddata data/model/tam.lstm
 
 #after flag -e the first path takes the pre-existing data it was trained on
